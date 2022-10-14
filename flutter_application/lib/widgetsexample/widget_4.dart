a5f32bcd848c473edf9c91f3eb4bf86b2f56cfb7
indieka900 <indiekaj@gmail.com>
2022-10-14 23:55:01 +0300

    Flutter widgets

--- flutter_application/lib/widgetsexample/widget_4.dart
+++ flutter_application/lib/widgetsexample/widget_4.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Showdialogw extends StatelessWidget {
+class Showdialogw extends StatefulWidget {
   const Showdialogw({super.key});
 
+  @override
+  State<Showdialogw> createState() => _ShowdialogwState();
+}
+
+class _ShowdialogwState extends State<Showdialogw> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,29 +15,40 @@ class Showdialogw extends StatelessWidget {
         title: const Text('Show alert dialog widget'),
       ),
       body: Center(
-        child: TextButton(
-          style: TextButton.styleFrom(
-              backgroundColor: Colors.black,
-              animationDuration: const Duration(seconds: 2),
-              elevation: 2),
-          onPressed: () {
-            showDialog(
-                context: context,
-                builder: (context) => SimpleDialog(
-                      title: const Text('Flutter Tutorial'),
-                      contentPadding: const EdgeInsets.all(22),
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Go Back'),
-                        )
-                      ],
-                      //content: const Text('This is the alert dialog'),
-                    ));
-          },
-          child: const Text('Show dialog message'),
+        child: Row(
+          children: [
+            TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  animationDuration: const Duration(seconds: 2),
+                  elevation: 2),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => SimpleDialog(
+                          title: const Text('Flutter Tutorial'),
+                          contentPadding: const EdgeInsets.all(22),
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Go Back'),
+                            )
+                          ],
+                          //content: const Text('This is the alert dialog'),
+                        ));
+              },
+              child: const Text('Show dialog message'),
+            ),
+            // GestureDetector(
+            //   onLongPress: () {
+            //     setState(() {
+            //       const Text('Long pressed');
+            //     });
+            //   },
+            // ),
+          ],
         ),
       ),
     );
