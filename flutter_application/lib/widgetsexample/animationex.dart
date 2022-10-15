9438a01760550a648becf775b3b2298205eb6a2c
indieka900 <indiekaj@gmail.com>
2022-10-15 22:33:33 +0300

    Flutter widgets

---
+++ flutter_application/lib/widgetsexample/animationex.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class FadeTest extends StatefulWidget {
+  const FadeTest({super.key});
+
+  @override
+  State<FadeTest> createState() => _FadeTestState();
+}
+
+class _FadeTestState extends State<FadeTest> with TickerProviderStateMixin {
+  late AnimationController controller;
+  late CurvedAnimation curve;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+    curve = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeIn,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animation test'),
+      ),
+      body: Center(
+        child: FadeTransition(
+          opacity: curve,
+          child: const FlutterLogo(size: 150),
+        ),
+      ),
+    );
+  }
+}
