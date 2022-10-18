8bef64af597fcd6d3259692ce19884c22e8518f6
indieka900 <indiekaj@gmail.com>
2022-10-18 12:00:37 +0300

    Flutter widgets

--- flutter_application/lib/widgetsexample/http_tutorial.dart
+++ flutter_application/lib/widgetsexample/http_tutorial.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/widgetsexample/set_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Myhttpexa extends StatefulWidget {
   const Myhttpexa({super.key});
@@ -40,45 +41,62 @@ class _MyhttpexaState extends State<Myhttpexa> {
       //   title: const Text('HTTP Request'),
       // ),
       backgroundColor: const Color.fromARGB(255, 240, 213, 116),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (result != 'Loading...')
-                    const Icon(
-                      Icons.location_on,
-                      color: Color.fromARGB(255, 139, 139, 139),
+      body: result == 'Loading...'
+          ? const Center(
+              child: SpinKitFadingCircle(
+                color: Color.fromARGB(255, 26, 1, 1),
+                size: 50.0,
+              ),
+              // child: SpinKitThreeInOut(
+              //   itemBuilder: (BuildContext context, int index) {
+              //     return DecoratedBox(
+              //       decoration: BoxDecoration(
+              //         color: index.isEven ? Colors.red : Colors.green,
+              //       ),
+              //       //child: const Text('Loading'),
+              //     );
+              //   },
+              // ),
+            )
+          : Center(
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        if (result != 'Loading...')
+                          const Icon(
+                            Icons.location_on,
+                            color: Color.fromARGB(255, 139, 139, 139),
+                          ),
+                        Text(
+                          locate,
+                          style: const TextStyle(
+                              fontFamily: 'Roboto',
+                              //fontWeight: FontWeight.w300,
+                              decoration: TextDecoration.underline,
+                              fontStyle: FontStyle.italic,
+                              color: Color.fromARGB(255, 15, 128, 148),
+                              fontSize: 18),
+                        ),
+                      ],
                     ),
-                  Text(
-                    locate,
-                    style: const TextStyle(
-                        fontFamily: 'Roboto',
-                        //fontWeight: FontWeight.w300,
-                        decoration: TextDecoration.underline,
-                        fontStyle: FontStyle.italic,
-                        color: Color.fromARGB(255, 15, 128, 148),
-                        fontSize: 18),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Text(
-                result,
-                style: const TextStyle(
-                  fontSize: 50,
-                  //fontStyle: FontStyle.italic,
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Text(
+                      result,
+                      style: const TextStyle(
+                        fontSize: 50,
+                        //fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
